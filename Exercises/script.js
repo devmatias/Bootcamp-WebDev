1fda4e6b4ded3774e26c1fe140d17742b1e744c3
devmatias <devmatiascff@gmail.com>
2022-10-26 15:43:46 -0300

    Mais alguns exercícios

--- Exercises/script.js
+++ Exercises/script.js
@@ -62,16 +62,37 @@
 
 // console.log(whoIsPaying(array));
 
-function beer(count) {
-    while(count >= 0) {
-        if(count !== 0) {
-            console.log(count + " bottles of beer on the wall," + count + " bottles of beer. Take 1 down, pass it around," + (count - 1) + " bottles on the wall.");
-        } else {
-            console.log("No more bottles of beer on the wall,no more bottles of beer. Go to the storenad buy some more, 99 bottles of beer on the wall.");
-        }
-        count -= 1;
+// function beer(count) {
+//     while(count >= 0) {
+//         if(count !== 0) {
+//             console.log(count + " bottles of beer on the wall," + count + " bottles of beer. Take 1 down, pass it around," + (count - 1) + " bottles on the wall.");
+//         } else {
+//             console.log("No more bottles of beer on the wall,no more bottles of beer. Go to the storenad buy some more, 99 bottles of beer on the wall.");
+//         }
+//         count -= 1;
         
+//     }
+// }
+
+// beer(100);
+
+function fibonacci(number) {
+    let array = [];
+    if(number > 2){
+        array = [0, 1];
+        for(let index = 1; index < number; index++){
+            let sum = array[index] + array[index - 1];
+            console.log(index);
+            array.push(sum);
+        }
+    } else if(number === 2) {
+        array = [0, 1];
+    } else if(number === 1){
+        array = [0];
+    } else {
+        console.log(`Invalid number`)
     }
+    return array;
 }
 
-beer(100);
+console.log(fibonacci(15));
