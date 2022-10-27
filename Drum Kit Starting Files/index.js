f1536589c33a073b32b8824082f343dd4e96be3b
devmatias <devmatiascff@gmail.com>
2022-10-27 13:49:26 -0300

    Add Drum project

---
+++ Drum Kit Starting Files/index.js
@@ -0,0 +1,7 @@
+for(let index = 0; index < document.querySelectorAll('.drum').length; index++){
+    document.querySelectorAll('.drum')[index].addEventListener('click', function () {
+        alert("I got clicked!");
+    });
+}
+
+
