7ee1664146c978ce090eec6d43d1c0fc0fff15df
devmatias <devmatiascff@gmail.com>
2022-11-07 12:49:40 -0300

    Drum kit project done

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,7 +1,58 @@
-for(let index = 0; index < document.querySelectorAll('.drum').length; index++){
-    document.querySelectorAll('.drum')[index].addEventListener('click', function () {
-        alert("I got clicked!");
-    });
+const getDrumButtons = document.getElementsByClassName("drum");
+
+const changeKeys = (myKey) => {
+  switch (myKey) {
+    case "w":
+      const crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+    case "a":
+      const kick = new Audio("sounds/kick-bass.mp3");
+      kick.play();
+      break;
+    case "s":
+      const snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+    case "d":
+      const tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+    case "j":
+      const tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+    case "k":
+      const tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+    case "l":
+      const tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+    default:
+      console.log(myKey);
+      break;
+  }
+};
+
+for (let index = 0; index < getDrumButtons.length; index++) {
+  getDrumButtons[index].addEventListener("click", function () {
+    const buttonInnerHTML = this.innerHTML;
+    changeKeys(buttonInnerHTML);
+    buttonAnimation(buttonInnerHTML);
+  });
 }
 
+document.addEventListener("keydown", (event) => {
+    changeKeys(event.key);
+    buttonAnimation(event.key);
+});
 
+const buttonAnimation = (currentkey) => {
+    const activeButton = document.querySelector('.' + currentkey);
+    activeButton.classList.add('pressed');
+    setTimeout(function() {
+        activeButton.classList.remove('pressed');
+    }, 100);
+}
